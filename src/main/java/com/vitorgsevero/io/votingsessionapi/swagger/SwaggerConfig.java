e59279881b253e3c54fa1d41a2c9204cd1630fbb
vitorgsevero <vitorgsevero@gmail.com>
2020-08-30 20:51:20 -0300

    Adding Swagger Config

--- src/main/java/com/vitorgsevero/io/votingsessionapi/swagger/SwaggerConfig.java
+++ src/main/java/com/vitorgsevero/io/votingsessionapi/swagger/SwaggerConfig.java
@@ -1,4 +1,38 @@
 package com.vitorgsevero.io.votingsessionapi.swagger;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDoc() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vitorgsevero.io"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Voting Session")
+                .description("This is the API docs for all project endpoints built.")
+                .version("1.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/license/LICENSE-2.0")
+                .build();
+    }
 }
